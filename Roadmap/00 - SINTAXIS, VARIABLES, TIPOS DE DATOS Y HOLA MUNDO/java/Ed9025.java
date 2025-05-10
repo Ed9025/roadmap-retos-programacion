33f76c2a671b6f22382a71586d66c17059cf9fca
Edwin Mejia <elopez75290@gmail.com>
2025-05-10 15:59:04 -0600

    #00 - Java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Ed9025.java
@@ -0,0 +1,19 @@
+public class Ed9025 {
+    public static void main(String[] args) {
+        //https://www.java.com/
+        /*Este es un comentario
+         * este es otro comentario
+         * este es un tercer comentario
+         */
+
+         // comentario en una linea
+         String variable = "variable";
+         int numero = 1;
+         float num = 3.45f;
+         char character = 'h';
+         boolean continuar = true;
+         final int numero4 = 5;
+         
+         System.out.println("Hola, Java!");
+    }
+}
---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Main.java
@@ -0,0 +1,24 @@
+public class Main{
+    public static void main(String[]args){
+            //https://www.java.com
+            /* 
+            Este es un comentario
+            este es otro comentar
+            y este otro es comentario
+            */
+
+            // Este es un comentario en una linea
+            String variable = "variable";
+            var numero = 1;
+            var precio = 3.2;
+            final int valor = 4;
+            int numero3 = 1;
+             double numero2= 3.5;
+            String variable2="variable";
+             float numero4 = 3.24f;
+             char caracter= 'H';
+             boolean continuar= true;
+            System.out.println("Hola, java!"); 
+    }    
+}
+
